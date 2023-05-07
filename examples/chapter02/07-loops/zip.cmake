8b0b59ad5f52a936b3e92f7024eeb458e41209d2
zc2799964726 <zc2799964726@163.com>
2023-05-07 15:47:34 +0800

    5月7日1，2章实践

--- examples/chapter02/07-loops/zip.cmake
+++ examples/chapter02/07-loops/zip.cmake
@@ -5,3 +5,10 @@ set(L2 "1;2;3;4;5")
 foreach(num IN ZIP_LISTS L1 L2)
     message(STATUS "num_0=${num_0}, num_1=${num_1}")
 endforeach()
+
+# foreach(<loop_var>... IN ZIP_LISTS <lists>)
+# cmake 为每个列表都创建了一个<loop_var>_<N>变量，用每个列表的项填充该变量
+
+foreach(EngNum AlaburNum IN ZIP_LISTS L1 L2)
+    message(STATUS "EngNum = ${EngNum}, AlaburNum = ${AlaburNum}")
+endforeach()
--- examples/chapter02/10-useful/message_context.cmake
+++ examples/chapter02/10-useful/message_context.cmake
@@ -1,12 +1,17 @@
 cmake_minimum_required(VERSION 3.20.0)
 
 function(foo)
-  list(APPEND CMAKE_MESSAGE_CONTEXT "foo")
+  #list(APPEND CMAKE_MESSAGE_CONTEXT "foo")
+  list(APPEND CMAKE_MESSAGE_INDENT " ")
   message("foo message")
 endfunction()
 
-list(APPEND CMAKE_MESSAGE_CONTEXT "top")
+#list(APPEND CMAKE_MESSAGE_CONTEXT "top")
 
 message("Before `foo`")
 foo()
 message("After `foo`")
+# 这里需要使用cmake -P chapter02/10-useful/message_context.cmake --log-context 
+# 当启用命令行标志cmake --log-context时，消息将装饰为点分隔的上下文，并存储在CMAKE_MESSAGE_CONTEXT列表中。
+# message()的另一个很酷的技巧是在CMAKE_MESSAGE_INDENT列表中添加缩进(与CMAKE_MESSAGE_CONTEXT的方法完全相同):
+# list(APPEND CMAKE_MESSAGE_INDENT " ")
